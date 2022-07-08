bb91b00ba11576ed460c542225c8c7bb36d95d8a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-08 21:30:19 +0300

    Key and StringLength eklendi.

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace EntityLayer.Concrete
 {
      public class Contact
     {
+        [Key]
         public int ContactID { get; set; }
+
+        [StringLength(50)]
         public string UserName { get; set; }
+
+        [StringLength(50)]
         public string UserMail { get; set; }
+
+        [StringLength(50)]
         public string Subject { get; set; }
+
+        //Burayı boş bıraktık. Bakalım boş bırakınca karşımıza nasıl gelicek.
         public string Message { get; set; }
     }
 }
