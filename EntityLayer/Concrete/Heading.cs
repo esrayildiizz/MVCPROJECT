d60e621078a1b82cca48e6a900a4af13a96cfe9a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-08 20:45:17 +0300

    Iliskiler olusturuldu.

--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-     public class Heading
+    public class Heading
     {
         public int HeadingID { get; set; }
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
 
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+        public ICollection<Content> Contents { get; set; }
+
+
+
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
     }
 }
