02d2182806cb5f2b687c07cf8f2a63b0bb8d25b4
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-17 14:54:12 +0300

    GenericRepository olusturuldu ve gerekli bilgiler girildi.

--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLayer.Abstract;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -10,29 +11,46 @@ namespace DataAccessLayer.Concrete.Repositories
 {
     public class GenericRepository<T> : IRepository<T> where T : class
     {
+
+        Context c = new Context();
+        DbSet<T> _object;
+
+
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
+
+
         public void Delete(T p)
         {
-            throw new NotImplementedException();
+            _object.Remove(p);
+            c.SaveChanges();
         }
 
         public void Insert(T p)
         {
-            throw new NotImplementedException();
+            _object.Add(p); //void tanımladığımız için return kullanmadık.
+            c.SaveChanges(); //Context de değişiklikleri kaydet.
+     
         }
 
         public List<T> List()
         {
-            throw new NotImplementedException();
+            return _object.ToList();
         }
 
         public List<T> List(Expression<Func<T, bool>> filter)
         {
-            throw new NotImplementedException();
+            return _object.Where(filter).ToList();
         }
 
         public void Update(T p)
         {
-            throw new NotImplementedException();
+            c.SaveChanges();
         }
     }
+
+      
+    
 }
