ac2ddb0b9c34c884c218e4ed04949877fdf78b42
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-17 13:25:23 +0300

    Tum siniflar icin interface olusturuldu.

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,10 @@ namespace DataAccessLayer.Abstract
         void Insert(T p);
         void Delete(T p);
         void Update(T p);
+
+        //Şartlı listeleme için aşağıdaki listeleme yöntemi kullanılır.
+        List<T> List(Expression<Func<T, bool>> filter);
+            
+          
     }
 }
