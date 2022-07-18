a20f7d0af3052593a2a14f22088023c1a65cf6bd
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-18 21:39:13 +0300

    Listeleme ve ekleme islemi eklendi.

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,24 @@ namespace BusinessLayer.Concrete
 {
      public class CategoryManager
     {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
 
+        public List<Category> GetAllBL() //BL: BusinessLayer 
+        {
+            return repo.List();
+        }
+
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)
+            {
+                //Hata mesajı içi ayrı bir sınıf oluşturacağız.
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+
+        }
     }
 }
