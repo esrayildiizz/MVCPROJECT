ca577cb8ef975e93f8c143777dd6861f09910436
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-18 21:24:38 +0300

    Concrete klosoru icine siniflar ekllendi.

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLayer
+namespace BusinessLayer.Concrete
 {
-    public class Class1
+     public class CategoryManager
     {
+
     }
 }
