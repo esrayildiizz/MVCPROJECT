513837e42816da1626c396887f33f769c2da006a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-19 21:10:17 +0300

    Yeni ActionResult eklendi.

--- MvcProject/Controllers/CategoryController.cs
+++ MvcProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,12 @@ namespace MvcProject.Controllers
         {
             var categoryvalues = cm.GetAllBL();
             return View(categoryvalues);
+        }
+
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
