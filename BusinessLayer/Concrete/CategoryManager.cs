aa8fd0db6ac1d508bbabd99515d40d62878e9669
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-20 16:51:22 +0300

    Listeleme icin dondurulecek deger yazildi.

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -19,31 +19,9 @@ namespace BusinessLayer.Concrete
             _categoryDal = categoryDal;
         }
 
-
-
-
-        // GenericRepository<Category> repo = new GenericRepository<Category>();
-
-        //public List<Category> GetAllBL() //BL: BusinessLayer 
-        //{
-        //    return repo.List();
-        //}
-
-        //public void CategoryAddBL(Category p)
-        //{
-        //    if(p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)
-        //    {
-        //        //Hata mesajı içi ayrı bir sınıf oluşturacağız.
-        //    }
-        //    else
-        //    {
-        //        repo.Insert(p);
-        //    }
-
-        //}
         public List<Category> GetList()
         {
-            return 
+            return _categoryDal.List();
         }
     }
 }
