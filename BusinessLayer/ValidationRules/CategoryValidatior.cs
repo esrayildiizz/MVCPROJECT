acf9929a5afeac606d3e14fbf6a8fe05b53263b0
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-21 15:12:14 +0300

    rules were written.

--- BusinessLayer/ValidationRules/CategoryValidatior.cs
+++ BusinessLayer/ValidationRules/CategoryValidatior.cs
@@ -10,6 +10,18 @@ namespace BusinessLayer.ValidationRules
 {
     public  class CategoryValidatior: AbstractValidator<Category>
     {
+        public CategoryValidatior()
+        {
+            //CategoryName boş olamaz.Ve bu mesaj önümüze gelir.
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz.");
 
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama Alanını Boş Geçemezsiniz.");
+
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karater girişi yapınız.");
+
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın.");
+
+
+        }
     }
 }
