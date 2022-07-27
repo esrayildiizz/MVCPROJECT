f981706d88190b94227beaa5cf1172038cef2a3e
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-27 13:12:43 +0300

    AddCategory has been added.

--- MvcProject/Controllers/AdminCategoryController.cs
+++ MvcProject/Controllers/AdminCategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,32 @@ namespace MvcProject.Controllers
         {
             var categoryvalues = cm.GetList();
             return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            ValidationResult results = categoryvalidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
