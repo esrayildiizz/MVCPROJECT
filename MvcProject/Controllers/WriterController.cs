8df896b41d17268ae9d55e229541e914db31ff3b
Esra YILDIZ <esrayildiizz@outlook.com>
2022-07-30 14:43:20 +0300

    Added AddWriter.

--- MvcProject/Controllers/WriterController.cs
+++ MvcProject/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +25,26 @@ namespace MvcProject.Controllers
         [HttpGet]
         public ActionResult AddWriter()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
