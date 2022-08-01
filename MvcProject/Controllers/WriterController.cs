a59b2236833bde65adb5f52c70e5e23d5945e24d
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-01 21:59:12 +0300

    ActionResult has been created.

--- MvcProject/Controllers/WriterController.cs
+++ MvcProject/Controllers/WriterController.cs
@@ -14,6 +14,7 @@ namespace MvcProject.Controllers
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator writerValidator = new WriterValidator();
         // GET: Writer
         public ActionResult Index()
         {
@@ -31,7 +32,7 @@ namespace MvcProject.Controllers
         [HttpPost]
         public ActionResult AddWriter(Writer p)
         {
-            WriterValidator writerValidator = new WriterValidator();
+            
             ValidationResult results = writerValidator.Validate(p);
             if (results.IsValid)
             {
@@ -58,8 +59,21 @@ namespace MvcProject.Controllers
         [HttpPost]
         public ActionResult EditWriter(Writer p)
         {
-            wm.WriterUpdate(p);
-            return RedirectToAction("Index");
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterUpdate(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
 
         }
     }
