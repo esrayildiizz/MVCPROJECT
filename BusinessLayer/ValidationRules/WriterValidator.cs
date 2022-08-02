a38db67ec95cb54a528dd3105797899344721922
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-02 23:15:11 +0300

    Editing done.

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -19,6 +19,8 @@ namespace BusinessLayer.ValidationRules
 
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Kısmını Boş Geçemezsiniz.");
 
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Ünvan Kısmını Boş Geçemezsiniz.");
+
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karater girişi yapınız.");
             
             RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayın.");
