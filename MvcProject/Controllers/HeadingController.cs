6434f207034cbc8c9991ec8081ff7acd8fccc7ef
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-03 19:26:35 +0300

    Added GetList.

--- MvcProject/Controllers/HeadingController.cs
+++ MvcProject/Controllers/HeadingController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,14 @@ namespace MvcProject.Controllers
 {
     public class HeadingController : Controller
     {
+
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
         // GET: Heading
         public ActionResult Index()
         {
-            return View();
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
         }
+        
     }
 }
