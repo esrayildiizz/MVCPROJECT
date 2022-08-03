8ad67ca0142399ed1d6455e69e6b5a5a58cfcfe5
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-03 14:03:32 +0300

    IHeadingService implemented.

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager : IHeadingService
+    {
+        IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)//yapıcı metot tanımladık.
+        {
+            _headingDal = headingDal;
+        }
+
+        public Heading GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Heading> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HeadingAdd(Heading heading)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
