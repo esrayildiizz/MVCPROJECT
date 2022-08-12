ff1490720d5f4ec9537337f91e77418017a872d8
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-12 11:47:02 +0300

    Method name edited.

--- BusinessLayer/Abstract/IContentService.cs
+++ BusinessLayer/Abstract/IContentService.cs
@@ -10,7 +10,7 @@ namespace BusinessLayer.Abstract
      public interface IContentService
     {
         List<Content> GetList();
-        List<Content> GetListByID(int id); //Id ye tüm listeyi döndür.
+        List<Content> GetListByHeadingID(int id); //Id ye tüm listeyi döndür.
         void ContentAdd(Content content);
         Content GetByID(int id);
         void ContentDelete(Content content);
