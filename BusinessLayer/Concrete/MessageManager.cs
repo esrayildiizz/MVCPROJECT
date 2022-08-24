8fccb470e1346edf8a509e629e73477c2e02e2d1
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-24 21:55:34 +0300

    The new method has been implemented.

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+     public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal) //Yapıcı metot tanımladık.
+        {
+            _messageDal = messageDal;
+        }
+
+        public Message GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetListInbox()
+        {
+            return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com");
+        }
+
+        public List<Message> GetListSendbox()
+        {
+            return _messageDal.List(x => x.SenderMail == "admin@gmail.com");
+        }
+
+        public void MessageAdd(Message message)
+        {
+            _messageDal.Insert(message);
+        }
+
+        public void MessageDelete(Message message)
+        {
+            _messageDal.Delete(message);
+        }
+
+        public void MessageUpdate(Message message)
+        {
+            _messageDal.Update(message);
+        }
+    }
+}
