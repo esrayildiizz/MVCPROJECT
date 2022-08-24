b93e8ab77a40e28a573e291dabfbde9ba780cf7b
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-24 21:57:11 +0300

    Added new ActionResult.

---
+++ MvcProject/Controllers/MessageController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageDal());
+
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var messagelist = mm.GetListInbox();
+            return View(messagelist);
+        }
+
+        public ActionResult Sendbox()
+        {
+            var messagelist = mm.GetListSendbox();
+            return View(messagelist);
+        }
+    }
+}
