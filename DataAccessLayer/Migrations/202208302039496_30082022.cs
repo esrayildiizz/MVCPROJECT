3c256ff95dd0597e512a9d1a69b832d0d360b8df
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-31 11:54:33 +0300

    Added new migration.

---
+++ DataAccessLayer/Migrations/202208302039496_30082022.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _30082022 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Messages", "MessageContent", c => c.String());
+            DropColumn("dbo.Messages", "MessageContact");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Messages", "MessageContact", c => c.String());
+            DropColumn("dbo.Messages", "MessageContent");
+        }
+    }
+}
