5158da84257e7caa1cfc0f6e43dabf64fff1e0b1
Esra YILDIZ <esrayildiizz@outlook.com>
2022-08-31 22:13:40 +0300

    Added ContactDate.

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -21,6 +21,8 @@ namespace EntityLayer.Concrete
         [StringLength(50)]
         public string Subject { get; set; }
 
+        public DateTime ContactDate { get; set; }
+
         //Burayı boş bıraktık. Bakalım boş bırakınca karşımıza nasıl gelicek.
         public string Message { get; set; }
     }
