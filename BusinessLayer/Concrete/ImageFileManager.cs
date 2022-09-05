c7464ddb085f95070230c39cc58b89378152d1e2
Esra YILDIZ <esrayildiizz@outlook.com>
2022-09-05 22:41:10 +0300

    The constructor method has been created.

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+     public class ImageFileManager:IImageFileService
+    {
+        IImageFileDal _imagefileDal;
+
+        public ImageFileManager(IImageFileDal imagefileDal)
+        {
+            _imagefileDal = imagefileDal;
+        }
+    }
+}
