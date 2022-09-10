5251e15f1ae0c419371858eb9fe878115e9fed38
Esra YILDIZ <esrayildiizz@outlook.com>
2022-09-10 16:35:41 +0300

    Added new ActionResult.

--- MvcProject/Controllers/WriterPanelController.cs
+++ MvcProject/Controllers/WriterPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +12,42 @@ namespace MvcProject.Controllers
     public class WriterPanelController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         // GET: WriterPanel
 
         public ActionResult WriterProfile()
         {
             return View();
         }
-        public ActionResult MyHeading(int id)
+        [AllowAnonymous]
+        public ActionResult MyHeading()
         {
-            id = 4;
-            var values = hm.GetListByWriter(id);
+            //id = 4;
+            var values = hm.GetListByWriter();
             return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 4;
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+          
         }
     }
 }
