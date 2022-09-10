d652a63dc36a078e4d33744e074b02fd7de74329
Esra YILDIZ <esrayildiizz@outlook.com>
2022-09-10 13:27:48 +0300

    Added new ActionResult(MyHeading).

--- MvcProject/Controllers/WriterPanelController.cs
+++ MvcProject/Controllers/WriterPanelController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,18 @@ namespace MvcProject.Controllers
 {
     public class WriterPanelController : Controller
     {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
         // GET: WriterPanel
-        public ActionResult Index()
+
+        public ActionResult WriterProfile()
         {
             return View();
+        }
+        public ActionResult MyHeading(int id)
+        {
+            id = 4;
+            var values = hm.GetListByWriter(id);
+            return View(values);
         }
     }
 }
