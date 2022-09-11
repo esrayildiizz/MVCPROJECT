45c9fa2c575b359832eaea8fba5c6cd18d345a6c
Esra YILDIZ <esrayildiizz@outlook.com>
2022-09-11 10:48:13 +0300

    Added new ActionResult(EditHeading).

--- MvcProject/Controllers/WriterPanelController.cs
+++ MvcProject/Controllers/WriterPanelController.cs
@@ -19,7 +19,7 @@ namespace MvcProject.Controllers
         {
             return View();
         }
-        [AllowAnonymous]
+      
         public ActionResult MyHeading()
         {
             //id = 4;
@@ -36,6 +36,7 @@ namespace MvcProject.Controllers
                                                       Text = x.CategoryName,
                                                       Value = x.CategoryID.ToString()
                                                   }).ToList();
+            ViewBag.vlc = valuecategory;
             return View();
         }
 
@@ -45,9 +46,32 @@ namespace MvcProject.Controllers
         {
             p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
             p.WriterID = 4;
+            p.HeadingStatus = true;
             hm.HeadingAdd(p);
-            return RedirectToAction("Index");
+            return RedirectToAction("MyHeading");
           
         }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("MyHeading");
+        }
+
     }
 }
