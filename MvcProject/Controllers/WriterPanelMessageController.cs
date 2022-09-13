f311d202f72a374a037cdd318b101e7993d72bdb
Esra YILDIZ <esrayildiizz@outlook.com>
2022-09-13 21:53:41 +0300

    Added new ActionResult.

--- MvcProject/Controllers/WriterPanelMessageController.cs
+++ MvcProject/Controllers/WriterPanelMessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,6 +45,27 @@ namespace MvcProject.Controllers
         {
             var values = ctm.GetByID(id);
             return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            ValidationResult results = messagevalidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("Sendbox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
 
     }
