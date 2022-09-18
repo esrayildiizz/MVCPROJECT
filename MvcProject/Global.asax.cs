c60d2fb50fd80922761a93d5f2d47b66dee2e569
Esra YILDIZ <esrayildiizz@outlook.com>
2022-09-18 16:23:29 +0300

    The Authorize process has been increased to the project level.

--- MvcProject/Global.asax.cs
+++ MvcProject/Global.asax.cs
@@ -12,6 +12,7 @@ namespace MvcProject
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
